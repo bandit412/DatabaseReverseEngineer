ce53547291d439fc26dfcee1f7bbf6bdf863205f
Allan Anderson <allana@shaw.ca>
2017-03-22 17:53:09 -0600

    Updated Entity classes
    Moved the using statements above the namespace definition

--- eRestaurantReverseEngineer/eRestaurantSystem.Data/Entities/RecipeIngredient.cs
+++ eRestaurantReverseEngineer/eRestaurantSystem.Data/Entities/RecipeIngredient.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
 namespace eRestaurantSystem.Data.Entities
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     public partial class RecipeIngredient
     {
         public int RecipeIngredientID { get; set; }
--- eRestaurantReverseEngineer/eRestaurantSystem.Data/Entities/SpecialEvent.cs
+++ eRestaurantReverseEngineer/eRestaurantSystem.Data/Entities/SpecialEvent.cs
@@ -1,11 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
 namespace eRestaurantSystem.Data.Entities
 {
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Data.Entity.Spatial;
-
     public partial class SpecialEvent
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
